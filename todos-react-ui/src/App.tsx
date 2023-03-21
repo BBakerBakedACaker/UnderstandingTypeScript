2a6a565dd476e8944c88b9c2a39daa2ee089ed7f
Brandon Baker <bbakerbakedacaker@gmail.com>
2023-03-21 14:08:13 -0500

    basic react todo app
    basic react todo app

--- todos-react-ui/src/App.tsx
+++ todos-react-ui/src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import TodoList from './components/TodoList';
+import { Todo } from './models/todoModel';
 
 const App: React.FC = () => {
+
+  // Creating a state to manage one todo
+  const [todo, setTodo] = useState<string>("");
+
+  // Creating a state to manage list of todos
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+
+  };
+
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputFeild></InputFeild>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
