1c0c513858f248a6aff3a914cf885fa0ea6065b1
Brandon Baker <bbakerbakedacaker@gmail.com>
2023-04-05 08:36:34 -0500

    Added about and Home routes
    Added about and Home routes

--- react-apollo-posts/src/components/app/app.component.tsx
+++ react-apollo-posts/src/components/app/app.component.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from '../header/header.component';
+import About from '../../pages/about/about.page';
+import Home from '../../pages/home/home.page';
 
+import '../../common/styles';
 import './app.component.css';
+
 
 const App: React.FC = () => {
     return (
         <Router>
             <Header />
+            <Routes>
+                <Route path='/' element={<Home />}>
+                </Route>
+                <Route path='/about' element={<About />}>
+                </Route>
+            </Routes>
         </Router>
     );
 }
--- react-apollo-posts/src/components/header/header.component.tsx
+++ react-apollo-posts/src/components/header/header.component.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './header.component.css';
+
 const Header: React.FC = () => {
     return (
         <header>
